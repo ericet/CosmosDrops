b4f2dad0757cf76c92fadda45d9ce2aba4d907c1
ericet <ericetchen@gmail.com>
2022-01-17 11:34:45 -0500

    updated airdrops

--- js/app.js
+++ js/app.js
@@ -9,6 +9,16 @@ const checkDesmos = (address) => {
         })
     });
 }
+const checkLikecoin = (address) => {
+    return new Promise((resolve, reject) => {
+        axios.get(`https://anyplace-cors.herokuapp.com/https://airdrop.like.co/api/overview?address=${address}`).then(res => {
+            console.log(res);
+            resolve(res.data);
+        }).catch(err => {
+            reject(err);
+        })
+    });
+}
 
 const checkGame = (address) => {
     return new Promise((resolve, reject) => {
@@ -87,12 +97,14 @@ $(document).ready(async function () {
         $('#spinner').html(`<div class="animationload">
         <div class="osahanloading"></div>
     </div>`);
-        Promise.all([checkDesmos(address), checkGame(address), checkStar(address), checkJuno(address)]).then(([desmos, game, star, juno]) => {
+        Promise.all([checkDesmos(address), checkGame(address), checkStar(address), checkJuno(address),checkLikecoin(address)]).then(([desmos, game, star, juno,likecoin]) => {
             $('#desmos').html(`The allocated $DSM amount is <br/><h3>${desmos.dsm_allotted.toFixed(2)} DSM</h3>`);
             $('#game').html(`The allocated $GAME amount is <br/><h3>${(game.amount / 10e5).toFixed(2)} GAME</h3>`);
             $('#stargaze').html(`The allocated $STAR amount is <br/><h3>${star.balance.toFixed(2)} STAR</h3>`);
             // $('#comdex').html(`The allocated $CMDX amount is <br/><h3>${comdex.length > 0 ? Number(comdex[0].drop).toFixed(2) : 0} CMDX</h3>`);
             $('#juno').html(`The allocated $JUNO amount is <br/><h3>${juno.balance.toFixed(2)} JUNO</h3>`);
+            $('#likecoin').html(`The allocated $LIKE amount is <br/><h3>${(likecoin.allocatedAmount / 1e9).toFixed(2)} LIKE</h3>`);
+
             // let pstakeHtml = '';
             // if (pstake.success) {
             //     let pstakeBalance = Number(pstake.result.airdrop.tokenHoldersAndStakersDrop) + Number(pstake.result.airdrop.stakeDropAuditStakersDrop) + Number(pstake.result.airdrop.stakeDropStakersDrop);
@@ -109,6 +121,7 @@ $(document).ready(async function () {
         $('#sunny').html(`<br/><a href="https://airdrop.sunny.ag/" class="btn btn-info">Check Now</a>`)
         $('#osmosis').html(`<br/><a href="https://app.osmosis.zone/airdrop" class="btn btn-info">Check Now</a>`)
         $('#lum').html(`<br/><a href="https://airdrop.lum.network/" class="btn btn-info">Check Now</a>`)
+        $('#sommelier').html(`<br/><a href="https://airdrop.sommelier.finance/" class="btn btn-info">Check Now</a>`)
 
 
     });
